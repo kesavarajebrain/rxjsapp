6631f73bee93027fc1a0143dacb6441f2c0907f2
Kesava Raj <kesavarajtry@gmail.com>
2024-06-15 23:52:09 +0530

    operators almost covered

--- src/app/observable-vs-subject/observable-vs-subject.component.ts
+++ src/app/observable-vs-subject/observable-vs-subject.component.ts
@@ -1,142 +1,219 @@
 import { Component, OnInit } from '@angular/core';
-import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
+import {
+  AsyncSubject,
+  BehaviorSubject,
+  Observable,
+  ReplaySubject,
+  Subject,
+  interval,
+} from 'rxjs';
 
 @Component({
   selector: 'app-observable-vs-subject',
   templateUrl: './observable-vs-subject.component.html',
-  styleUrl: './observable-vs-subject.component.css'
+  styleUrl: './observable-vs-subject.component.css',
 })
-export class ObservableVsSubjectComponent implements OnInit{
-
+export class ObservableVsSubjectComponent implements OnInit {
   observableRandomArr: any[] = [];
   subjectRandomArr: any[] = [];
   behaviourSubjectRandomArr: any[] = [];
   replaySubjectRandomArr: any[] = [];
-  asyncSubjectRandomArr: any[] =[];
+  asyncSubjectRandomArr: any[] = [];
+  counterObservable: any[] = [];
 
-
+  subscriberOne: any;
   ngOnInit(): void {
     // observable
-    const myObservable = new Observable(observer => {
+    const myObservable = new Observable((observer) => {
       // generate random values
       observer.next(Math.random());
     });
 
     // subscribe those values multiple times - every console log we receive random values
-    myObservable.subscribe((data)=>{
+    myObservable.subscribe((data) => {
       this.observableRandomArr.push(data);
       console.log(data);
     });
 
-    myObservable.subscribe((data)=>{
+    myObservable.subscribe((data) => {
       this.observableRandomArr.push(data);
       console.log(data);
     });
 
     // here we added settimout also
-    setTimeout(()=>{
-      myObservable.subscribe((data)=>{
+    setTimeout(() => {
+      myObservable.subscribe((data) => {
         this.observableRandomArr.push(data);
         console.log(data);
       });
-    },1000)
- 
-    // subject 
+    }, 1000);
+
+    // subject
     const mySubject = new Subject();
     // subscribe those values multiple times - every console log we unique random values
-    mySubject.subscribe((data)=>{
-      this.subjectRandomArr.push(data)
+    mySubject.subscribe((data) => {
+      this.subjectRandomArr.push(data);
       console.log(data);
     });
 
-    mySubject.subscribe((data)=>{
-      this.subjectRandomArr.push(data)
+    mySubject.subscribe((data) => {
+      this.subjectRandomArr.push(data);
       console.log(data);
     });
 
-    mySubject.subscribe((data)=>{
-      this.subjectRandomArr.push(data)
+    mySubject.subscribe((data) => {
+      this.subjectRandomArr.push(data);
       console.log(data);
     });
 
     // add set timeout - its not print
-    setTimeout(()=>{
-      mySubject.subscribe((data)=>{
-        this.subjectRandomArr.push(data)
+    setTimeout(() => {
+      mySubject.subscribe((data) => {
+        this.subjectRandomArr.push(data);
         console.log(data);
       });
-    },2000)
+    }, 2000);
     // generate random values
     mySubject.next(Math.random());
 
+    // behavioursubject
+    const myBehavioursubject = new BehaviorSubject<any>(100);
+    // initial value is 100 after the next method again it called but now updated value as 200 so its display
+    // subscribe bs
+    myBehavioursubject.subscribe((data) => {
+      this.behaviourSubjectRandomArr.push(
+        'BehaviorSubject Subscriber 1 -> ' + data
+      );
+      console.log('BehaviorSubject Subscriber 1-> ' + data);
+    });
 
-     // behavioursubject
-  const myBehavioursubject = new BehaviorSubject<any>(100);
-  // initial value is 100 after the next method again it called but now updated value as 200 so its display
-  // subscribe bs
-  myBehavioursubject.subscribe((data)=>{
-    this.behaviourSubjectRandomArr.push('BehaviorSubject Subscriber 1 -> '+ data);
-    console.log('BehaviorSubject Subscriber 1-> '+data);
-  });
+    myBehavioursubject.subscribe((data) => {
+      this.behaviourSubjectRandomArr.push(
+        'BehaviorSubject Subscriber 2 -> ' + data
+      );
+      console.log('BehaviorSubject Subscriber 2 -> ' + data);
+    });
 
-  myBehavioursubject.subscribe((data)=>{
-    this.behaviourSubjectRandomArr.push('BehaviorSubject Subscriber 2 -> '+ data);
-    console.log('BehaviorSubject Subscriber 2 -> '+data);
-  });
+    myBehavioursubject.next(200); // next means where ever we subscribed those its update new value
 
-  myBehavioursubject.next(200); // next means where ever we subscribed those its update new value
+    myBehavioursubject.subscribe((data) => {
+      this.behaviourSubjectRandomArr.push(
+        'BehaviorSubject Subscriber 3 -> ' + data
+      );
+      console.log('BehaviorSubject Subscriber 3-> ' + data);
+    });
 
-  myBehavioursubject.subscribe((data)=>{
-    this.behaviourSubjectRandomArr.push('BehaviorSubject Subscriber 3 -> ' + data);
-    console.log('BehaviorSubject Subscriber 3-> '+data);
-  });
+    // 100, 100 , 200 ,200,200 - initially 100 is for sub 1, sub2 , after next we emit 200 so sub3 have new value after that sub1,2 receive new value
 
-  // 100, 100 , 200 ,200,200 - initially 100 is for sub 1, sub2 , after next we emit 200 so sub3 have new value after that sub1,2 receive new value
+    // replay subject
+    // initial value optional
+    // when using replay subject -> all the subscribers receive old emitted value and new values
+    const myReplaySubject = new ReplaySubject<any>();
+    myReplaySubject.next(100);
 
-  // replay subject
-  // initial value optional
-  // when using replay subject -> all the subscribers receive old emitted value and new values
-  const myReplaySubject = new ReplaySubject<any>(); 
-  myReplaySubject.next(100);
-  
-  myReplaySubject.subscribe((data)=>{
-    console.log(data);
-    this.replaySubjectRandomArr.push('Replay Subscriber 1 -> '+data);
-  });
+    myReplaySubject.subscribe((data) => {
+      console.log(data);
+      this.replaySubjectRandomArr.push('Replay Subscriber 1 -> ' + data);
+    });
 
-  myReplaySubject.subscribe((data)=>{
-    console.log(data);
-    this.replaySubjectRandomArr.push('Replay Subscriber 2 -> '+data);
-  });
+    myReplaySubject.subscribe((data) => {
+      console.log(data);
+      this.replaySubjectRandomArr.push('Replay Subscriber 2 -> ' + data);
+    });
 
-  myReplaySubject.next(200);
+    myReplaySubject.next(200);
 
-  myReplaySubject.subscribe((data)=>{
-    console.log(data);
-    this.replaySubjectRandomArr.push('Replay Subscriber 3 -> '+data);
-  });
+    myReplaySubject.subscribe((data) => {
+      console.log(data);
+      this.replaySubjectRandomArr.push('Replay Subscriber 3 -> ' + data);
+    });
 
-  // 100,100,200,200,100,200 - here sub1,2 receive 100 after emit 200 sub1,2 receive 200 then sub3 receive both 100 and 200 
+    // 100,100,200,200,100,200 - here sub1,2 receive 100 after emit 200 sub1,2 receive 200 then sub3 receive both 100 and 200
 
-  // async subject 
-  const myAsyncSubject = new AsyncSubject<any>();
+    // async subject
+    const myAsyncSubject = new AsyncSubject<any>();
 
-  myAsyncSubject.next('A');
-  myAsyncSubject.next('B');
-  myAsyncSubject.next('C');
+    myAsyncSubject.next('A');
+    myAsyncSubject.next('B');
+    myAsyncSubject.next('C');
 
-  myAsyncSubject.complete(); // MUST it complete
-  // async subject only returns last emitted value after that complete method only , if not complete method it wont able to subscribe the data
-  myAsyncSubject.subscribe((data)=>{
-    console.log('Async Subject -> 1 ' + data); 
-    this.asyncSubjectRandomArr.push('Async Subject 1 -> ' +data)
-  });
+    myAsyncSubject.complete(); // MUST it complete
+    // async subject only returns last emitted value after that complete method only , if not complete method it wont able to subscribe the data
+    myAsyncSubject.subscribe((data) => {
+      console.log('Async Subject -> 1 ' + data);
+      this.asyncSubjectRandomArr.push('Async Subject 1 -> ' + data);
+    });
 
-  myAsyncSubject.next('D'); // it wont emit because it already complete method happened
+    myAsyncSubject.next('D'); // it wont emit because it already complete method happened
 
-  myAsyncSubject.subscribe((data)=>{
-    console.log('Async Subject -> 2 ' + data); 
-    this.asyncSubjectRandomArr.push('Async Subject 2 -> ' +data)
-  });
+    myAsyncSubject.subscribe((data) => {
+      console.log('Async Subject -> 2 ' + data);
+      this.asyncSubjectRandomArr.push('Async Subject 2 -> ' + data);
+    });
+
+    // promise vs observable
+
+    // Promise
+    // Native js code
+    // Called eager
+    const myPromise = new Promise((resolve, reject) => {
+      console.log('Promise called eager');
+      resolve(100);
+      resolve(200); // emits only single value
+      resolve(300); // emits only single value
+      reject('Error Occured');
+    });
+
+    // access promise
+    myPromise
+      .then((data) => {
+        console.log('Promise Data => ' + data); // get data if in resolve state
+      })
+      .catch((err) => {
+        console.log(err); // get error if in reject state
+      });
+
+    // observable
+    // from Rxjs
+    // called lazy
+
+    const myNewObservable = new Observable((observer) => {
+      console.log('Observable called when its subscribed');
+      observer.next(10); // emits multiple values
+      observer.next(20); // emits multiple values
+      observer.next(30); // emits multiple values
+      observer.complete();
+      observer.error('Error occured ');
+    });
+
+    // access observable
+    myNewObservable.subscribe({
+      next(x) {
+        console.log('Observable Data=> ' + x);
+      },
+      error(err) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });
+  }
+
+  // unsubscribe
+  counter = interval(1000); // its rxjs interval its also a observable - gives data every 1sec
+
+  // access counter observable
+  subscribe() {
+    this.subscriberOne = this.counter.subscribe((data) => {
+      this.counterObservable.push(data);
+      console.log("Subcribed!");
+      console.log('counter data => ' + data);
+    });
+  }
+
+  unSubscribe() {
+    console.log("Unsubcribed!");
+    this.subscriberOne.unsubscribe();
   }
 }
--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, filter, from, map, of } from 'rxjs';
+import {
+  Observable,
+  concat,
+  count,
+  defaultIfEmpty,
+  distinct,
+  filter,
+  find,
+  findIndex,
+  first,
+  from,
+  interval,
+  last,
+  map,
+  max,
+  merge,
+  min,
+  of,
+  range,
+  skip,
+  take,
+  tap,
+  toArray,
+  zip,
+} from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -18,11 +42,36 @@ export class RxjsComponent implements OnInit {
   operartorMapArr: any[] = [];
   operartorFilterArr: any[] = [];
   commonArr: any[] = [1, 2, 3, 4, 5];
+  commonArrNames: any[] = [
+    'Kurinji',
+    'Mullai',
+    'Marudham',
+    'Neithal',
+    'Paalai',
+  ];
   //of
   ofArray = [1, 2, 3, 4, 5, 'hai', { name: 'test' }];
   //from
   fromArray = [1, 2, 3, 4, 'hello'];
   newObservable!: Observable<any>;
+  operatorRangeArr: any[] = [];
+  operatorDistinctArr: any[] = [];
+  operatorFirstArr: any[] = [];
+  operatorLastArr: any[] = [];
+  operatorSkipArr: any[] = [];
+  operatorTakeArr: any[] = [];
+  operatorIntervalArr: any[] = [];
+  operatorToArrayArr: any[] = [];
+  operartorTapArr: any[] = [];
+  operartorConcatArr: any[] = [];
+  operartorMergeArr: any[] = [];
+  operartorZipArr: any[] = [];
+  operatordefaultIfEmptyArr: any;
+  operatorFindArr: any[] = [];
+  operatorFindIndexArr: any[] = [];
+  operatorCountArr: any[] = [];
+  operatorMin?: number;
+  operatorMax?: number;
   ngOnInit(): void {
     this.fruits = new Observable(function (observer) {
       try {
@@ -181,6 +230,7 @@ export class RxjsComponent implements OnInit {
   // map operator - use pipe for this
   mapObservable = from(this.commonArr);
   mapOperator() {
+    //include another operator like map so we use pipe
     this.mapObservable
       .pipe(
         map((val) => {
@@ -195,6 +245,7 @@ export class RxjsComponent implements OnInit {
   // filter operator - use pipe for this
   filterObservable = from(this.commonArr);
   filterOperator() {
+    //include another operator like filter so we use pipe
     this.filterObservable
       .pipe(
         filter((val, i) => {
@@ -205,5 +256,211 @@ export class RxjsComponent implements OnInit {
       .subscribe((data) => {
         this.operartorFilterArr.push(data);
       });
+  }
+
+  // range operator
+  rangeOperator() {
+    // range (starting number , no of items from the starting number)
+    const rangeObservable = range(10, 2);
+    rangeObservable.subscribe((val) => {
+      this.operatorRangeArr.push(val);
+      console.log('Range =>' + val);
+    });
+  }
+
+  // distinct operator
+  distinctOperator() {
+    // here declared one array with duplicates values, for the each value itration we used from operator
+    const distinctArr = from([
+      1, 2, 1, 1, 3, 4, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 8, 88, 9,
+    ]);
+    //include another operator like distinct so we use pipe the we subscribed - its takes only unique values
+    distinctArr.pipe(distinct()).subscribe((val) => {
+      this.operatorDistinctArr.push(val);
+    });
+  }
+
+  // first operator
+  firstOperator() {
+    // here declared one arr with 5 names - this operator emits the first value
+    const firstOperatortArr = from(this.commonArrNames);
+    firstOperatortArr.pipe(first()).subscribe((val) => {
+      this.operatorFirstArr.push(val);
+    });
+  }
+
+  // last operator
+  lastOperator() {
+    // here declared one arr with 5 names - this operator emits the last value
+    const lastOperatortArr = from(this.commonArrNames);
+    lastOperatortArr.pipe(last()).subscribe((val) => {
+      this.operatorLastArr.push(val);
+    });
+  }
+
+  // skip operator
+  skipOperator() {
+    // here declared one arr with 5 names - skip operator skips from the start in the given position
+    const skipOperatortArr = from(this.commonArrNames);
+    skipOperatortArr.pipe(skip(3)).subscribe((val) => {
+      this.operatorSkipArr.push(val);
+    });
+  }
+
+  // take operator
+  takeOperator() {
+    const takeOperatortArr = from(this.commonArrNames);
+    // take operator takes values from the starting, in the given position
+    takeOperatortArr.pipe(take(6)).subscribe((val) => {
+      this.operatorTakeArr.push(val);
+    });
+  }
+
+  // inteval
+  intervalOperator() {
+    // interval operator keep on trigger with the given interval of time
+    const intervalArr = interval(1000);
+    // this is keep on trigger because we are not add condition
+    intervalArr.subscribe((val) => {
+      this.operatorIntervalArr.push(val);
+    });
+    // here we can combine take operator also
+    // intervalArr.pipe((take(5))).subscribe((val) => {
+    //   this.operatorIntervalArr.push(val);
+    // });
+  }
+
+  // toArray operator
+  toArrayOperator() {
+    const dataArr = of(1, 2, 3, 'Hello', 'world!');
+    // toArray operator format data into array format
+    dataArr.pipe(toArray()).subscribe((val) => {
+      console.log('toArray operator => ', val);
+      this.operatorToArrayArr.push(val);
+    });
+  }
+
+  // tap operator
+  tapObservable = from(this.commonArr);
+  tapOperator() {
+    //include another operator like map so we use pipe
+    this.tapObservable
+      .pipe(
+        // tap operator used to there is modification of original data
+        tap((val) => {
+          return val * 10;
+        })
+      )
+      .subscribe((data) => {
+        this.operartorTapArr.push(data);
+      });
+  }
+
+  // concat operator
+  concatOperator() {
+    const Arr = from(this.commonArrNames);
+    // first operator - operator 2
+    const operator2 = Arr.pipe(first());
+    // last operator - operator 1
+    const operator1 = Arr.pipe(last());
+    //take operator - operator 3
+    const operator3 = Arr.pipe(take(3));
+    // concat operator concat multiple operators , with one subscribe its all the operators data
+    const concatOpertor = concat(operator1, operator2, operator3);
+    concatOpertor.subscribe((val) => {
+      this.operartorConcatArr.push(val);
+    });
+  }
+
+  // merge operator
+  mergeOperator() {
+    const Arr = from(this.commonArrNames);
+    // first operator - operator 2
+    const operator2 = Arr.pipe(first());
+    // last operator - operator 1
+    const operator1 = Arr.pipe(last());
+    //take operator - operator 3
+    const operator3 = Arr.pipe(take(3));
+    // concat operator concat multiple operators , with one subscribe its all the operators data
+    const concatOpertor = merge(operator1, operator2, operator3, 3);
+    concatOpertor.subscribe((val) => {
+      this.operartorMergeArr.push(val);
+    });
+  }
+
+  // zip operator
+  zipOperator() {
+    let age$ = of(18, 31, 25, 43);
+    let name$ = of('Agran', 'Aruvi', 'Thenmozhi', 'Sembian');
+    let city$ = of('Maduari', 'Chennai', 'Trichy', 'Thanjavur');
+    let status$ = of(true, false, true, false);
+    // zip operator forms data if all the values matched in the given order
+    // map operator formats
+    zip(name$, age$, city$, status$)
+      .pipe(map(([name, age, city, status]) => ({ name, age, city, status })))
+      .subscribe((val) => {
+        this.operartorZipArr.push(val);
+        console.log(val);
+      });
+  }
+
+  // defaultIfEmpty operator
+  defaultIfEmptyOperator() {
+    const defaultArr = of(1, 2, 3);
+    // this operator used to if there is no data or there is no matched data based on the condition it will emit what we given
+    defaultArr
+      .pipe(
+        filter((x) => x > 5),
+        defaultIfEmpty('There is no matches or its empty')
+      )
+      .subscribe((val) => {
+        console.log(val);
+        this.operatordefaultIfEmptyArr = val;
+      });
+  }
+
+  // find operators
+  findOperator() {
+    const dataArr = of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    // find operator emits only one data with the satisfied condition
+    dataArr.pipe(find((x) => x % 2 === 0)).subscribe((val) => {
+      this.operatorFindArr.push(val);
+    });
+  }
+
+  // findIndex operator
+  findIndexOperator() {
+    const dataArr = of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    // findIndex operator emits only one data with the satisfied condition's position
+    dataArr.pipe(findIndex((x) => x % 2 === 0)).subscribe((val) => {
+      this.operatorFindIndexArr.push(val);
+    });
+  }
+
+  // count operator
+  countOperator() {
+    const dataArr = of(12, 43, 42, 56, 22, 12, 44, 90);
+    // count operator emits how many counts
+    dataArr.pipe(count()).subscribe((val) => {
+      this.operatorCountArr.push(val);
+    });
+  }
+
+  // min operator
+  minOperator() {
+    const dataArr = of(12, 43, 42, 56, 22, 12, 44, 90);
+    // min operator emits min value
+    dataArr.pipe(min()).subscribe((val) => {
+      this.operatorMin =val;
+    });
+  }
+
+   // min operator
+   maxOperator() {
+    const dataArr = of(12, 43, 42, 56, 22, 12, 44, 90,100);
+    // min operator emits maximum value
+    dataArr.pipe(max()).subscribe((val) => {
+      this.operatorMax =val;
+    });
   }
 }
