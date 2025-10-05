af9c1abbafbbda1fdd744e98890301d14481adad
KESAVA RAJ.R <kesavaraj.try@gmail.com>
2025-10-05 12:07:59 +0530

    added og tags and adsense

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
+}
+declare var adsbygoogle: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  constructor(private router: Router) {
+     // Reload AdSense ads on route change
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        try {
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (e) {
+          console.log('AdSense reload error:', e);
+        }
+      });
+  }
 }
